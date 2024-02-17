df64da8c11914051969ca02a366e5be1fb824e84
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-02-17 12:39:07 +0100

    esercizio finito con bonus 2

---
+++ db.php
@@ -0,0 +1,21 @@
+<?php
+$archivio = [
+    [
+        'title' => 'Rambo',
+        'production' => 'Lions Gate Entertainment',
+        'duration' => '114',
+        'genre' => ['Azione', 
+                    'Guerra'],
+        'oscar' => '1',
+        'goldenGlobes' => '2',
+    ],
+    [
+        'title' => 'Lord of tyhe ring',
+        'production' => 'New Line Cinema',
+        'duration' => '221',
+        'genre' => ['Fantasy',
+                    'Avventura'],
+        'oscar' => '4',
+        'goldenGlobes' => '3',
+    ]
+]; 
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . '/class.php';
+
+$movie1 = new Movie(0);
+var_dump($movie1);
+$movie2 = new Movie(1);
+var_dump($movie2);
+
+
+?>
+
+
