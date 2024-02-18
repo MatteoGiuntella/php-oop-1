d611e9da7749b7d5119dcbc7dfe485d7b452da92
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-02-18 11:59:11 +0100

    erroe risolto esercizio finito

--- class.php
+++ class.php
@@ -1,5 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+
 class Movie
 {
 
@@ -9,59 +10,48 @@ class Movie
     public $genre;
     public $oscar;
     public $goldenGlobes;
-    public $prizes;
 
-    // function __construct($titolo, $produzione, $durata, Genere $genere, $statuettaoro, $globo)
-    // {
-    //     $this->title = $titolo;
-    //     $this->genre = $genere;
-    //     $this->production = $produzione;
-    //     $this->duration = $durata;
-    //     $this->oscar = $statuettaoro;
-    //     $this->goldenGlobes = $globo;
-    //     $this->setPrizes();
-    // }
-    function __construct($index)
+
+    function __construct($titolo, $produzione, $durata, $genere, $statuettaoro, $globo)
     {
-        require __DIR__ . '/db.php';
-        for ($i = 0; $i < count($archvio); $i++) {
-            $this->title = $archvio[$index]['title'];
-            $this->genre = $archvio[$index]['genre'];
-            $this->production = $archvio[$index]['production'];
-            $this->duration = $archvio[$index]['duration'];
-            $this->oscar = $archvio[$index]['oscar'];
-            $this->goldenGlobes = $archvio[$index]['goldenGlobes'];
-            $this->setPrizes($archvio[$index]['oscar'], $archvio[$index]['goldenGlobes']);
-        }
+        $this->title = $titolo;
+        $this->genre = $genere;
+        $this->production = $produzione;
+        $this->duration = $durata;
+        $this->oscar = $statuettaoro;
+        $this->goldenGlobes = $globo;
+        
     }
+  
 
     public function setPrizes($oscar , $goldenGlobes)
     {
-        $this->prizes = intval($oscar) + intval($goldenGlobes);
+        $prizes = intval($oscar) + intval($goldenGlobes);
+        return $prizes;
         
     }
 };
-// class Genere
-// {
-//     public $genere1;
-//     public $genere2;
+class Genere
+{
+    public $genere1;
+    public $genere2;
 
-//     function __construct($tipo1, $tipo2)
-//     {
-//         $this->genere1 = $tipo1;
-//         $this->genere2 = $tipo2;
-//     }
-// }
+    function __construct($tipo1, $tipo2)
+    {
+        $this->genere1 = $tipo1;
+        $this->genere2 = $tipo2;
+    }
+}
 
-// $generefilm1 = new Genere('Azione', 'Guerra');
+$generefilm1 = new Genere('Azione', 'Guerra');
 
-// $generefilm2 = new Genere('Fantasy', 'Avventura');
+$generefilm2 = new Genere('Fantasy', 'Avventura');
 
-// $rambo = new Movie('Rambo', 'Lions Gate Entertainment', '114', $generefilm1, 1, 2);
+$rambo = new Movie('Rambo', 'Lions Gate Entertainment', '114', $generefilm1, 1, 2);
 
-// $lordofthering = new Movie('The Lord of the ring', 'New Line Cinema', '221', $generefilm2, 4, 3);
+$lordofthering = new Movie('The Lord of the ring', 'New Line Cinema', '221', $generefilm2, 4, 3);
 
 
-// var_dump($lordofthering);
-// var_dump($rambo);
+var_dump($lordofthering);
+var_dump($rambo);
 ?>
--- index.php
+++ index.php
@@ -1,23 +1,71 @@
 <?php
-header("Access-Control-Allow-Origin: *");
 require __DIR__ . '/class.php';
-
-$movie1 = new Movie(0);
-var_dump($movie1);
-$movie2 = new Movie(1);
-var_dump($movie2);
-
+$jsonarchivio = file_get_contents('./db.json');
+$archivio = json_decode($jsonarchivio, true);
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    
-</body>
-</html>
 
+<body>
+    <?php
+    for ($i = 0; $i < count($archivio); $i++) {
+        $titolo = $archivio[$i]['title'];
+        $genere = $archivio[$i]['genre'];
+        $produzione = $archivio[$i]['production'];
+        $durata = $archivio[$i]['duration'];
+        $statuettaoro = $archivio[$i]['oscar'];
+        $globo = $archivio[$i]['goldenGlobes'];
+        $movie[$i] = new Movie($titolo, $produzione, $durata, $genere, $statuettaoro, $globo)
+
+    ?>
+        <ul>
+            <li>
+                <?php
+                echo $titolo;
+                ?>
+            </li>
+            <li>
+                <?php
+                echo implode(', ' , $genere);
+                ?>
+            </li>
+            <li>
+                <?php
+                echo $produzione;
+                ?>
+            </li>
+            <li>
+                <?php
+                echo $durata;
+                ?>
+            </li>
+            <li>
+                <?php
+                echo $statuettaoro;
+                ?>
+            </li>
+            <li>
+                <?php
+                echo $globo;
+                ?>
+            </li>
+            <li>
+                <?php
+                echo $movie[$i]->setPrizes($statuettaoro, $globo);
+                ?>
+            </li>
+
+        </ul>
+    <?php
+    }
+    ?>
+</body>
+
+</html>
